df00d1a0fe9b1fe6966cbe7cd75d1c6b36681892
Justin Prasad <52518975+justinePrasad@users.noreply.github.com>
2021-03-09 23:39:16 -0500

    Create excercise3-2.cpp
    Exercise for 3-2: converts miles to kilometers

---
+++ chp3/excercises/excercise3-2.cpp
@@ -0,0 +1,10 @@
+#include "../../../../std_lib_facilities.h""
+
+int main()
+{
+	double miles;
+	cout << "Enter miles to be converted to kilometers: ";
+	cin >> miles;
+	
+	double km = miles * 1.609;
+	cout << miles << " miles is " << km << " kilometers \n";

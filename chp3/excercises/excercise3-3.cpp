3cd8379ffb06748a38a11973681e97e04ac3e683
Justin Prasad <52518975+justinePrasad@users.noreply.github.com>
2021-03-10 10:15:14 -0500

    Create excercise3-3.cpp
    Ex 3-3:  variables with legal and illegal names to see how the compiler reacts

---
+++ chp3/excercises/excercise3-3.cpp
@@ -0,0 +1,11 @@
+#include "../../../../std_lib_facilities.h"
+//header file from Programming-_Principles_and_Practice_Using_Cpp
+
+int main()
+{
+ //some key words are reserved, and identifier can't start with numbers
+	char 3 = 'a';
+   int double = 0;
+   char int = '1';
+   char send = 'a';
+}

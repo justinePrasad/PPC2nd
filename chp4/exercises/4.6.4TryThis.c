bf2d1e2a6c5e6c8d35f19c599c5e38f455afff93
Justin Prasad <52518975+justinePrasad@users.noreply.github.com>
2021-03-18 20:18:34 -0400

    Update 4.6.4TryThis.c
    Revamped being able to input disliked words, to make the program more dynamic

--- chp4/exercises/4.6.4TryThis.c
+++ chp4/exercises/4.6.4TryThis.c
@@ -3,8 +3,19 @@
 int main()
 {	
 	vector <string> words;
-	vector <string> disliked = { "qew", "qwe" };
+	vector <string> disliked;
 	
+	cout << "Enter a list of words you dislike\n" << "Enter ;: to terminate\n";
+	for (string temp; cin >> temp;)
+	{
+		if (temp == ";:")
+		{
+			break;
+		}
+		disliked.push_back(temp);
+	}
+
+	cout << "Enter a list of words\n";
 	for (string temp; cin >> temp;)
 		words.push_back(temp);
 	
@@ -16,6 +27,7 @@ int main()
 				cout << "BLEEP\n";
 			else
 				cout << words[i] << "\n";
+			break;
 		}
 	}
 	return 0;

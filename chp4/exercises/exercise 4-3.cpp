0859be3c9a1a42ac538fe02ffcf67ab8bac767b6
Justin Prasad <52518975+justinePrasad@users.noreply.github.com>
2021-03-27 15:24:29 -0400

    Update exercise 4-3.cpp
    Added way to find the total sum, and renamed variable name of sum to mean

--- chp4/exercises/exercise 4-3.cpp
+++ chp4/exercises/exercise 4-3.cpp
@@ -1,14 +1,20 @@
 #include "../../../../std_lib_facilities.h"
 
 int main()
-{
+{	
+	double sum = 0;
 	vector <double> distances;
+
 	for (double distance; cin >> distance;)
 		distances.push_back(distance);
 	sort(distances);
-  
+
+	for (auto temp : distances) {
+		sum += temp;
+	}
+	cout << "Sum of all values is " << sum << "\n";
 	cout << "Smallest Distance: " << distances[0] << "\n"
 		<< "Greatest Distance: " << distances[distances.size() - 1] << "\n";
-	double sum = (distances[0] + distances[distances.size() - 1]) / 2;
-	cout << "The mean between the two distances is " << sum << "\n";
+	double mean = (distances[0] + distances[distances.size() - 1]) / 2;
+	cout << "The mean between the two distances is " << mean << "\n";
 }
